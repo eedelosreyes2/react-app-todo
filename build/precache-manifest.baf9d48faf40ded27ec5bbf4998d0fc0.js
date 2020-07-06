c580ed0dc8198c124ffc5b87f3f370f0b0cd945d
eedelosreyes2 <eedelosreyes2@dons.usfca.edu>
2020-07-06 15:19:03 -0700

    build

---
+++ build/precache-manifest.baf9d48faf40ded27ec5bbf4998d0fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebff2d056310ab38438ed33d6becb3b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c76820c714c2b4531a4",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "e5acd19eefcc8394fc1c",
+    "url": "/static/js/2.a907c49f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a907c49f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c76820c714c2b4531a4",
+    "url": "/static/js/main.dec53265.chunk.js"
+  },
+  {
+    "revision": "de8e20b679451655391c",
+    "url": "/static/js/runtime-main.75e5761e.js"
+  }
+]);
